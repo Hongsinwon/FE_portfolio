f5b177e85fd6144ea39399d56fa215b769efb845
Hongsinwon <ghdtlsdnjs12@naver.com>
2026-06-24 00:37:15 +0900

    style : Profile 반응형 스타일 수정

--- src/components/profile.tsx
+++ src/components/profile.tsx
@@ -3,16 +3,6 @@
 import Image from "next/image";
 import style from "./profile.module.css";
 import profile from "../../public/sinwon.jpeg";
-// import htmlIcon from "../../public/skill/html5.webp";
-// import cssIcon from "../../public/skill/css3.webp";
-// import jsIcon from "../../public/skill/JS.webp";
-// import reactIcon from "../../public/skill/react.webp";
-// import nextIcon from "../../public/skill/next.webp";
-// import typescriptIcon from "../../public/skill/typescript.webp";
-// import dartIcon from "../../public/skill/dart.webp";
-// import flutterIcon from "../../public/skill/flutter.webp";
-// import gitIcon from "../../public/skill/git.png";
-// import githubIcon from "../../public/skill/github.webp";
 import { usePathname } from "next/navigation";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBlogger, faGithub } from "@fortawesome/free-brands-svg-icons";
@@ -71,28 +61,12 @@ export default function Profile() {
           </div>
         </div>
       </div>
-      {/* TODO : 메인 추가내용 */}
-      {/* <div className={style.skill}>
-        <div className={style.title}>SKILL</div>
-        <div className={style.skillIcon}>
-          <Image src={htmlIcon} alt="html5" width={30} />
-          <Image src={cssIcon} alt="css3" width={30} />
-          <Image src={jsIcon} alt="javascript" width={30} />
-          <Image src={reactIcon} alt="react" width={30} />
-          <Image src={nextIcon} alt="next" width={30} />
-          <Image src={typescriptIcon} alt="typescript" width={30} />
-          <Image src={dartIcon} alt="dart" width={30} />
-          <Image src={flutterIcon} alt="flutter" width={30} />
-          <Image src={gitIcon} alt="git" width={30} />
-          <Image src={githubIcon} alt="githubIcon" width={30} />
-        </div>
-      </div> */}
 
       <div className={style.introduction}>
         {/* TODO : 내용 수정 */}
         <div>
           <p className={style.infoTitle}>INTRODUCE</p>
-          <p className={style.content}>
+          <p className={`${style.content} ${style.pcMode}`}>
             웹·앱 서비스 프론트엔드 개발을 중심으로 다양한 스타트업에서
             <br /> 제품과 서비스를 개발해왔습니다. 개발뿐 아니라 기획과
             디자인에도 <br />
@@ -105,6 +79,15 @@ export default function Profile() {
             앞으로는 프론트엔드 전문성을 강화하는 동시에
             <br />
             비즈니스와 사용자 관점을 함께 고려하는 개발자로 성장하고자 합니다.
+          </p>
+          <p className={`${style.content} ${style.tabletMode}`}>
+            웹·앱 서비스 프론트엔드 개발을 중심으로 다양한 스타트업에서 제품과
+            서비스를 개발해왔습니다. 개발뿐 아니라 기획과 디자인에도 참여하며
+            서비스 전반을 이해하고 개선하는 경험을 쌓았습니다. 단순한 기능
+            구현을 넘어 사용자 경험과 서비스 방향성을 고민하며 문제 해결에
+            적극적으로 참여해왔습니다. 앞으로는 프론트엔드 전문성을 강화하는
+            동시에 비즈니스와 사용자 관점을 함께 고려하는 개발자로 성장하고자
+            합니다.
           </p>
         </div>
       </div>
